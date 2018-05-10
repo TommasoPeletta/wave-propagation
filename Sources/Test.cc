8d2a4ff60aeb321acb5f8d42387068284f630d08
AdrienCHABERT <adrien.chabert@etu.unige.ch>
2018-05-10 16:21:49 +0200

    Test

--- Sources/Test.cc
+++ Sources/Test.cc
@@ -1,8 +1,11 @@
 #include <iostream>
 using namespace std;
 
+//Pour lancer le code : g++ Test.cc -o Test
+//                      ./Test
+
 int main()
 {
-    cout << "Hello, World!";
+    cout << "Hello, World!\n";
     return 0;
 }
