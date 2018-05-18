7ce73a698ce34204ac225b509bc0add24af6b144
staufga0 <Guy-Raphael.Stauffer@etu.unige.ch>
2018-05-18 17:40:43 +0200

    source continue(variant avec le temps) et point réléchissant implémentés

--- Sources/2d_wave_propagation.cc
+++ Sources/2d_wave_propagation.cc
@@ -2,6 +2,7 @@
 #include <vector>
 #include <cmath>
 #include <algorithm>
+
 using std::vector;
 using namespace std;
 
@@ -11,6 +12,7 @@ using namespace std;
 const int sizeX = 10;
 const int sizeY = 10;
 const int q = 4;
+const double pi = 3.14159265358979323846;
 
 typedef double type_F[sizeX][sizeY][q+1];
 typedef double type_coeff_reffrac[sizeX][sizeY];
@@ -106,7 +108,7 @@ void afficher(type_F matrix) { //Fonction pour afficher une matrice
 //}
 }
 
-void foutComputation(double n[sizeX][sizeY], double v, double vi[q+1][2], double f_in[sizeX][sizeY][q+1] ){
+void foutComputation(double n[sizeX][sizeY], double v, double vi[q+1][2], double f_in[sizeX][sizeY][q+1], int iteration){
   type_F f_out;
   //calcul de vj = vi * j
   double rho;
@@ -114,22 +116,42 @@ void foutComputation(double n[sizeX][sizeY], double v, double vi[q+1][2], double
 //  double nn = 0.4; //!!!!!!
   for (int i = 0; i < sizeX; i++){
     for (int j = 0; j < sizeY; j++){
-      rho = rhoComputation(f_in,i,j);
-      double j_sum[2];
-      jComputation(j_sum,vi,f_in,i,j);
+      if (n[i][j]>=1) {
+        rho = rhoComputation(f_in,i,j);
+        double j_sum[2];
+        jComputation(j_sum,vi,f_in,i,j);
 
-      for (int k = 0; k < q+1; k++){
-        double vi_aux[2];
-        vi_aux[0] = vi[k][0];
-        vi_aux[1] = vi[k][1];
-        double vj = vectors_prod(vi_aux,j_sum);
-        if (k != 0){
-          f_out[i][j][k] = 2/(pow(n[i][j],2)* q) * rho + (1/v) * vj - f_in[i][j][k];//+ (1/v) * vj
-          //f_out[i][j][k] = 2/(pow(nn,2)* q) * rho - f_in[i][j][k];
-        } else {
-          f_out[i][j][k] = 2*(pow(n[i][j],2) - 1)/(pow(n[i][j],2)) * rho - f_in[i][j][k];
-          //f_out[i][j][k] = 2*(pow(nn,2) - 1)/(pow(nn,2)) * rho - f_in[i][j][k];
+        for (int k = 0; k < q+1; k++){
+          double vi_aux[2];
+          vi_aux[0] = vi[k][0];
+          vi_aux[1] = vi[k][1];
+          double vj = vectors_prod(vi_aux,j_sum);
+          if (k != 0){
+            f_out[i][j][k] = 2/(pow(n[i][j],2)* q) * rho + (1/v) * vj - f_in[i][j][k];//+ (1/v) * vj
+            //f_out[i][j][k] = 2/(pow(nn,2)* q) * rho - f_in[i][j][k];
+          } else {
+            f_out[i][j][k] = 2*(pow(n[i][j],2) - 1)/(pow(n[i][j],2)) * rho - f_in[i][j][k];
+            //f_out[i][j][k] = 2*(pow(nn,2) - 1)/(pow(nn,2)) * rho - f_in[i][j][k];
+          }
         }
+      } else if(n[i][j]>0){       //in this programme, sources are represented by a refraction coefficient between 0 and 1
+        double Amplitude = 30;
+        double phase = 0;
+        double facteur = 0.04;       // facteru must be <= 1 to assure lambda/deltaX > 10
+        double frequenceXtemps = 5*facteur*iteration*v/q;      //la frequence fois le temps (représenté par deltaT * iteration)
+        for (int k = 0; k<=q ; k++){
+          f_out[i][j][k] = Amplitude*sin(2*pi*frequenceXtemps);
+          //cout << 2*pi*frequenceXtemps << "  ==>  " <<  f_out[i][j][k] << "       ";
+        }
+      } else {
+        //reflecting surfaces represented by a negative refraction coefficient
+
+        f_out[i][j][0] = -f_in[i][j][0];
+        f_out[i][j][1] = -f_in[i][j][3];
+        f_out[i][j][2] = -f_in[i][j][4];
+        f_out[i][j][3] = -f_in[i][j][1];
+        f_out[i][j][4] = -f_in[i][j][2];
+
       }
     }
   }
@@ -161,21 +183,24 @@ int main() {
     double v = deltaX/deltaT;
     double vi[q+1][2] = {{0,0},{v,0},{0,v},{-v,0},{0,-v}};
 
-    vector<std::array<int, 4> vectSource;
-    int tmp[2] = {5,5};
-    vectSource.push_back(tmp);
+    //vector<std::array<int, 4> vectSource;
+    //int tmp[2] = {5,5};
+    //vectSource.push_back(tmp);
 
-    f_in[2][3][0] = 0;
+    /*f_in[2][3][0] = 0;
     f_in[2][3][1] = 0;
     f_in[2][3][2] = 0;
     f_in[2][3][3] = 0;
-    f_in[2][3][4] = 50;
+    f_in[2][3][4] = 50;*/
+    tabl_n[5][5] = 0.5;       // le point [5, 5] est une source
+    tabl_n[5][6] = -1;        // le point [5, 6] est une surface réfléchissante
     afficher(f_in);
     //foutComputation(tabl_n,v,vi,f_in);
     //cout << "ETAPE 1 : " << endl;
     //afficher(f_in);
-    for (int i = 1; i <= 8;i++){
-      foutComputation(tabl_n,v,vi,f_in);
+    for (int i = 1; i <= 5;i++){
+      foutComputation(tabl_n,v,vi,f_in,i);
+      cout << "\n";
       afficher(f_in);
     }
     cout << tabl_n[2][2] << "\n";
